fbdafe24a4fe6ce8957facfc05bf6742f069bd32
antoncab <48969643+arcpac@users.noreply.github.com>
2024-01-18 20:33:26 +1100

    7

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,20 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('password_reset_tokens', function (Blueprint $table) {
-            $table->increments('temporary_id')->first();
-        });
-
+       
         // Drop the existing primary key
-        Schema::table('password_reset_tokens', function (Blueprint $table) {
-            $table->dropPrimary(['email']);
-        });
-
-        // Rename the temporary column to 'email' and set it as the primary key
-        Schema::table('password_reset_tokens', function (Blueprint $table) {
-            $table->renameColumn('temporary_id', 'email');
-            $table->primary('email');
-        });
     }
 
     /**
