f69dafdef51085c48a5411f0910d72690635fa22
antoncab <48969643+arcpac@users.noreply.github.com>
2024-01-18 18:53:54 +1100

    update schema

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,6 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
+            // Drop the primary key if it exists
+            $table->dropPrimary(['email']);
+
+            // Define columns
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
