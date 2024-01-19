1f4b316db9d7c7e26f3eea35071a06a414da9f23
KC Marie Arce <arcekc@gmail.com>
2024-01-19 20:38:07 +1100

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Task Trackrz</title>
+    <title>Task Trackr</title>
 
     <!-- Favicon -->
     <link rel="icon" href="./public/favicon.ico" type="image/x-icon"/>
