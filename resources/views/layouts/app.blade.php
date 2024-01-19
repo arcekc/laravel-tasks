1430aa3586b69f045f4e5ec7fd6478ccdaddbed7
KC Marie Arce <arcekc@gmail.com>
2024-01-19 20:48:35 +1100

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,15 +9,17 @@
     <title>Task Trackr</title>
 
     <!-- Favicon -->
-    <link rel="icon" href="./public/favicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="./public/favicon.ico" type="image/x-icon" />
 
     <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet'
+        type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    {{--
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
         body {
@@ -29,7 +32,7 @@
 
         /* Dark Mode Styles */
         body.dark-mode {
-            background-color: #333;
+            background-color: #252525;
             color: #fff;
         }
 
@@ -41,15 +44,17 @@
         }
     </style>
 </head>
+
 <body id="app-layout">
     <nav class="navbar navbar-default">
         <div class="container">
-            <div class="navbar-header">
-                <!-- Branding Image -->
+            <div class="navbar-header btn-purple">
+                <!-- Branding Image with Logo -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List
+                    <img src="{{ asset('white-logo.png') }}" alt="Logo" style="height: 30px; width: auto;">
                 </a>
             </div>
+
             <!-- Dark Mode Toggle Button -->
             <button id="darkModeToggle" onclick="toggleDarkMode()">
                 🌙
@@ -62,7 +67,8 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    {{--
+    <script src="{{ elixir('js/app.js') }}"></script> --}}
 
     <script>
         // Function to toggle dark mode
@@ -71,4 +77,5 @@
         }
     </script>
 </body>
+
 </html>
