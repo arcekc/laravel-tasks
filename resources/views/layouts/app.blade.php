e2338489d2f2b656ef64cf9843f259a1611a39b5
KC Marie Arce <arcekc@gmail.com>
2024-01-19 20:14:47 +1100

    dark/light mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 
     <title>Task Trackr</title>
 
-     <!-- Favicon -->
-     <link rel="icon" href="../public/favicon.ico" type="image/x-icon"/>
+    <!-- Favicon -->
+    <link rel="icon" href="../public/favicon.ico" type="image/x-icon"/>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -26,19 +26,34 @@
         .fa-btn {
             margin-right: 6px;
         }
+
+        /* Dark Mode Styles */
+        body.dark-mode {
+            background-color: #333;
+            color: #fff;
+        }
+
+        /* Moon/Sun Toggle Styles */
+        #darkModeToggle {
+            cursor: pointer;
+            font-size: 20px;
+            margin: 10px;
+        }
     </style>
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
-
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Task List
                 </a>
             </div>
-
+            <!-- Dark Mode Toggle Button -->
+            <button id="darkModeToggle" onclick="toggleDarkMode()">
+                🌙
+            </button>
         </div>
     </nav>
 
@@ -48,5 +63,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <script>
+        // Function to toggle dark mode
+        function toggleDarkMode() {
+            document.body.classList.toggle('dark-mode');
+        }
+    </script>
 </body>
 </html>
