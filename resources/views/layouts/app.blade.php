ed097b03bcedd92a1f8abd53b941644095489f40
KC Marie Arce <arcekc@gmail.com>
2024-01-19 20:12:19 +1100

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
 
     <title>Task Trackr</title>
 
+     <!-- Favicon -->
+     <link rel="icon" href="../public/favicon.ico" type="image/x-icon"/>
+
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
